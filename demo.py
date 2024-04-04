caeb78f4a45e6a192e9eff3be55739eeda9f48e2
T-Yashwanth <yashwanthtelukuntla@gmail.com>
2024-04-04 15:10:08 -0500

    exception

--- demo.py
+++ demo.py
@@ -1,3 +1,8 @@
 from us_visa_prediction.logger import logging
+from us_visa_prediction.exception import USvisaException
+import sys
 
-logging.info("This is Logging test")
+try:
+    a = (1/0)
+except Exception as e:
+    logging.info(e)
